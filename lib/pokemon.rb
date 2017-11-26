8343d538dc0be9b5b555da28899c0352a401dbd0
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-26 17:47:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save(name, type, database_connection)
+    pokemon = database_connection.execute("INSERT INTO pokemon (name,type) VALUES (?, ?)", name, type)
+    @@all << pokemon
+  end
+
+  def self.find(id, db)
+    @@all.detect do |pokemon|
+      pokemon.id = id
+    end
+  end
+
 end
